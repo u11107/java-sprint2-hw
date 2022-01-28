71ed05f424f483f4918a9191b2c142e6b9ffe3fa
Rinat <eenot84@yandex.ru>
2022-01-28 23:53:27 +0300

    feat: Добавлено четыре класса -Manager -Task -Epic -SubTask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
+
+
+public class Main  {
+
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+
+
+
+
     }
+
 }
+
---
+++ src/ru/task/manager/Manager/Manager.java
@@ -0,0 +1,18 @@
+package ru.task.manager.Manager;
+
+import ru.task.manager.Task.Task;
+
+import java.util.HashMap;
+
+public class Manager<Epic,SubTask> {
+
+    HashMap<Integer, Task> tasks = new HashMap<Integer, Task>();
+    HashMap<Integer, Epic> epic = new HashMap<Integer, Epic>();
+    HashMap<Integer, SubTask> subTask = new HashMap<Integer, SubTask>();
+
+
+
+
+
+
+}
