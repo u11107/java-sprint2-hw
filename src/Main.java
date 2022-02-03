ae4c5b85cb97b378d1f7999355b2e2097e1c73fd
Rinat <eenot84@yandex.ru>
2022-02-03 11:16:02 +0300

    feat: Добавлено класс -managerMethods методы -addTask -printTaskAll -gettingById -clearTask -removeTaskId -updateTask

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,10 @@ public class Main  {
 
 
         Tasks tes1 = new Tasks(manager.generateId(),"Помыть 11тарелки и вилки", "Купить тарелки и вилки", Status.IN_PROGRESS);
-        manager.addTask(tes1);
+        manager.createTasks(tes1);
         Tasks tes2 = new Tasks(manager.generateId(),"Помыть тарелки и вилки", "Купить 123тарелки и вилки", Status.NEW);
-        manager.addTask(tes2);
-        manager.addTask(tes1);
+        manager.createTasks(tes2);
+        manager.createTasks(tes1);
         printAll();
         manager.removeTaskId(1);
         manager.updateTask(new Tasks(2,"jhbdsfjksd", "ihwefiwef", Status.NEW));
@@ -21,12 +21,13 @@ public class Main  {
 
         Epic ep4 = new Epic(manager.generateId(), "Помыть1", "выкинуть");
         Epic ep3 = new Epic(manager.generateId(), "Помыть", "выкинуть");
-        manager.addEpic(ep3);
-        manager.addEpic(ep4);
+        manager.createEpics(ep3);
+        manager.createEpics(ep4);
         printAll();
 
-        SubTasks ep1 = new SubTasks(manager.generateId(),"23Проснуться", "Встать",3);
-        manager.addSubtask(ep1);
+        SubTasks ep1 = new SubTasks(manager.generateId(),"23Проснуться", "Встать",Status.NEW,2323);
+        manager.createSubtask(ep1);
+        manager.addSubTaskEpic(ep1);
         printAll();
 
         manager.removeEpicId(3);
